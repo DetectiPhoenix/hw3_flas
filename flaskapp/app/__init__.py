0cbffac0d07987a07e3c346b37bd71792a5801d6
Kyo Lee <kyo.se.lee@gmail.com>
2022-03-31 14:50:43 -0700

    Deleted flaskapp/, moved files outside.

--- flaskapp/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from config import Config
-
-myapp_obj = Flask(__name__)
-myapp_obj.config.from_mapping(
-	SECRET_KEY = "PASSWORD"
-)
-
-from app import routes
--- flaskapp/app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-class LoginForm(FlaskForm):
-	cityName = StringField("City Name")
-	submit = SubmitField("Submit")
